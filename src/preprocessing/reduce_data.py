bc96750cf8d48b6fbeb351554174601b071fd301
Assa KEBE <assakebe06@gmail.com>
2026-06-22 14:12:01 +0200

    Reduce the data

--- src/preprocessing/reduce_data.py
+++ src/preprocessing/reduce_data.py
@@ -1,1 +1,5 @@
-# Script pour le prétraitement des données
+import pandas as pd
+
+df_sample = pd.read_csv("data/raw/flights.csv", nrows=200000)
+
+df_sample.to_csv("data/raw/sample_200k.csv", index=False)
